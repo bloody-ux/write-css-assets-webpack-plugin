cbd2f408c1c5cb5b82509e4fca143f65a11e1486
那达 <youxiang.lyx@alipay.com>
2018-07-02 15:50:39 +0800

    added functionality

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
-## what does it do
+## 这个plugin的作用
 
-webpack plugin to write css chunk and assets mapping to manifest.js file
+用于将webpack stats中的关于css的chunk/assets的映射关系输出到`manifest.js`文件的头部:
 
-keep manifest in bundle
+``` javascript
+
+window.__CSS_CHUNKS__= {"components/Async":"/components/Async.css","main":"/main.css"};
+
+
+```
+
+一般会和`extract-css-chunks-webpack-plugin`插件配合使用，更多可以参考`react-universal-component`以及`babel-plugin-universal-import`
+
+
+## 如何使用
+
+``` javascript
+plugins: [
+  new WriteCssAssetsPlugin()
+]
+```
+
+目前这个插件做得很简单，没有参数可以控制行为
--- index.js
+++ index.js
@@ -2,17 +2,33 @@
 
 const { ConcatSource } = require("webpack-sources");
 
-class WriteCssAssetsWebpackPlugin {
-	constructor() {
-		
-	}
+function createCssHash({
+  assetsByChunkName,
+  publicPath
+}) {
+  return Object.keys(assetsByChunkName).reduce((hash, name) => {
+    if (!assetsByChunkName[name] || !assetsByChunkName[name].find) return hash;
 
+    const file = assetsByChunkName[name].find(f => f.endsWith('.css'));
+    if (file) hash[name] = `${publicPath}${file}`;
+    return hash;
+  }, {});
+}
+
+class WriteCssAssetsWebpackPlugin {
 	apply(compiler) {
-		compiler.hooks.compilation.tap("WriteCssAssetsWebpackPlugin", compilation => {
-			compilation.hooks.optimizeChunkAssets.tap("WriteCssAssetsWebpackPlugin", chunks => {
-				console.log(compilation);
-				console.log(chunks);
-		});
+    compiler.plugin('emit', (compilation, callback) => {
+      const manifest = compilation.assets['manifest.js'];
+
+      if (manifest) {
+        const stats = compilation.getStats().toJson();
+        const cssHash = createCssHash(stats);
+        const hashString = `window.__CSS_CHUNKS__= ${JSON.stringify(cssHash)};`;
+        compilation.assets['manifest.js'] = new ConcatSource(hashString, '\n', manifest);
+      }
+      
+      callback();
+    });
 	}
 }
 
